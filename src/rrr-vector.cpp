36766db0222108e43db79ba52ebd7218476c7b69
jvtoppa <jvtoppa@gmail.com>
2026-04-09 01:57:06 -0300

    fix: bug with the way the tables were calculated

--- src/rrr-vector.cpp
+++ src/rrr-vector.cpp
@@ -12,11 +12,11 @@ bitVector B(string s)
     size_t n = s.size();
     bitVector bv = bitVector(n * 8, 8);
 
-    for(size_t c : s)
+    for (size_t c : s)
     {
-        for(int i = 7; i >= 0; i--)
+        for (int i = 7; i >= 0; i--)
         {
-            if((c >> i) & 1)
+            if ((c >> i) & 1)
             {
                 bv.append1();
             }
@@ -26,48 +26,49 @@ bitVector B(string s)
             }
         }
     }
- return bv;
-
+    return bv;
 }
 
 // There are 2^t possible bitvectors. Each of them must be saved to a class based
 // on no. of zeros.
-pair<vector<vector<bitVector>>, unordered_map<string, size_t>> create_tables(size_t t) // access: table[class][block] 
+pair<vector<vector<bitVector>>, unordered_map<string, size_t>> create_tables(size_t t) // access: table[class][block]
 {
     vector<vector<bitVector>> table(t + 1);
     unordered_map<string, size_t> pattern_to_offset;
 
     size_t total = 1 << t;
-    
+    bitVector block = bitVector(t, t);
+    for (size_t i = 0; i < t; i++)
+    {
+        block.append0();
+    }
+
     for (size_t x = 0; x < total; x++)
     {
-        bitVector block = bitVector(t, t);
         string pattern = "";
-        // inserts x inside of a bitvector
-        
-        for (size_t j = 0; j < t; j++)
+        for (size_t i = 0; i < t; i++)
         {
-            size_t bit = (x >> j) & 1;
+            block.set0(i);
+        }
+
+        for (size_t j = t; j > 0; j--)
+        {
+            size_t bit = (x >> (j - 1)) & 1;
             if (bit == 1)
             {
-
-                block.set1(t - 1 - j);
                 pattern += "1";
+                block.set1(t - j);
             }
-            else
-            {   
 
-                block.set0(t - 1 - j);
+            else
                 pattern += "0";
-            }
         }
-        
 
         // Counts how many ones in bitvector
         // to choose the class
-        
+
         size_t clss = 0;
-        for (size_t j = 0; j < t; j++) 
+        for (size_t j = 0; j < t; j++)
         {
             if (block[j] == 1)
                 clss++;
@@ -75,45 +76,39 @@ pair<vector<vector<bitVector>>, unordered_map<string, size_t>> create_tables(siz
         size_t offset = table[clss].size();
         table[clss].push_back(block);
         pattern_to_offset[pattern] = offset;
-    }        
+    }
 
     return {table, pattern_to_offset};
 }
 
-
-
 vector<size_t> K(bitVector bv, size_t t)
-{   
+{
     size_t n = bv.getLength();
-    vector<size_t> v; 
+    vector<size_t> v;
     size_t counter = 0;
 
     for (size_t i = 0; i < n; i++)
     {
         auto bit = bv[i];
-        
-        if(bit == 1)
-        counter++;
-        
-        if((i + 1)% t == 0 || i == n - 1)
+
+        if (bit == 1)
+            counter++;
+
+        if ((i + 1) % t == 0 || i == n - 1)
         {
             v.push_back(counter);
             counter = 0;
         }
-
-        
     }
-    
+
     for (auto item : v)
     {
         cout << item << " ";
     }
-    
 
     return v;
-
 }
-vector<size_t> superblock(const vector<size_t>& K, size_t factor, size_t t)
+vector<size_t> superblock(const vector<size_t> &K, size_t factor, size_t t)
 {
     size_t n = K.size();
     vector<size_t> superblock;
@@ -121,33 +116,31 @@ vector<size_t> superblock(const vector<size_t>& K, size_t factor, size_t t)
     for (size_t i = 0; i < n; i += factor * t)
     {
         size_t global_rank = 0;
-        
+
         for (size_t j = i; j < min(i + factor * t, n); j++)
         {
             global_rank += K[j];
         }
-        
+
         superblock.push_back(global_rank);
     }
-    
+
     return superblock;
 }
 
-
-
-vector<size_t> R(bitVector bv, unordered_map<string, size_t>& map, size_t t)
+vector<size_t> R(bitVector bv, unordered_map<string, size_t> &map, size_t t)
 {
     size_t n = bv.getLength();
     string block = "";
     vector<size_t> R;
-    
+
     for (size_t i = 0; i < n; i++)
     {
         if (bv[i] == 1)
             block += "1";
         else
             block += "0";
-        
+
         if ((i + 1) % t == 0)
         {
             R.push_back(map[block]);
@@ -167,10 +160,9 @@ vector<size_t> R(bitVector bv, unordered_map<string, size_t>& map, size_t t)
     {
         cout << R[i] << " ";
     }
-    
+
     return R;
 }
-
 
 void rank(size_t i, vector<size_t> superblock, vector<size_t> K, size_t t, size_t factor)
 {
@@ -182,13 +174,11 @@ void rank(size_t i, vector<size_t> superblock, vector<size_t> K, size_t t, size_
     {
         sum += K[i];
     }
-    
-    //Repeat previous step until we reach . We then add (from , not the global rank)
-    // to our result, where , and is the position we are querying local to.
-    // Our final answer is the result.
+
+    // Repeat previous step until we reach . We then add (from , not the global rank)
+    //  to our result, where , and is the position we are querying local to.
+    //  Our final answer is the result.
 }
-
-
 
 int main(void)
 {
@@ -200,7 +190,8 @@ int main(void)
     vector<size_t> Ka = K(bv, 4);
     vector<size_t> Ra = R(bv, maps.second, 4);
 
-    cout << "maps: ";
-    maps.first[Ra[0]][Ka[0]].print(); 
+    cout << "maps: " << Ka[0] << Ra[0] << "\n";
+    maps.first[Ra[2]][Ka[2]].print();
+
     return 0;
 }
